212bb4c92e7fec6d68b065ebe1ae5b2ab165610b
ablankholm <bo.bojsen@gmail.com>
2016-09-16 15:03:42 +0100

    Namespace character auto-conversion cleanup

--- src/SwipeTime-Core/Program.cs
+++ src/SwipeTime-Core/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 
-namespace SwipeTime_Core
+namespace SwipeTime.Core
 {
     public class Program
     {
