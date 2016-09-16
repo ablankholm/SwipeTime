87e010f0033e21b16139249c3999bef9544aed1d
ablankholm <bo.bojsen@gmail.com>
2016-09-16 15:01:35 +0100

    More namespace character auto-conversion cleanup

--- src/SwipeTime-Core/Startup.cs
+++ src/SwipeTime-Core/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace SwipeTime_Core
+namespace SwipeTime.Core
 {
     public class Startup
     {
