8d2a6ad9fb53e8511f4b124778ab1007a0cd1e88
ablankholm <bo.bojsen@gmail.com>
2016-09-16 14:57:41 +0100

    Namespace character auto-conversion cleanup

--- src/SwipeTime-Core/Controllers/HomeController.cs
+++ src/SwipeTime-Core/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace SwipeTime_Core.Controllers
+namespace SwipeTime.Core.Controllers
 {
     public class HomeController : Controller
     {
@@ -15,14 +15,14 @@ namespace SwipeTime_Core.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Under construction.";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Under construction.";
 
             return View();
         }
